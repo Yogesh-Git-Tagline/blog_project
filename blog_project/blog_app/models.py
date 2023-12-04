7d352b60005df87d54688dd31e587b4a2452667c
YogeshC <yogeshc.tagline.com>
2023-12-04 11:36:37 +0530

    worked on 2nd point

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
 
+
 class BlogDate(models.Model):
-    created_at=models.DateTimeField(auto_now_add=True)
-    modified_at=models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        abstract=True
+        abstract = True
+
 
 class Blog(BlogDate):
-    title=models.CharField(max_length=500)
-    author=models.ForeignKey(User,on_delete=models.CASCADE)
-    content=models.TextField()
+    title = models.CharField(max_length=500)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blogs')
+    content = models.TextField()
 
     class Meta:
         ordering = ('-created_at',)
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,31 +1,35 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import TemplateView,ListView,DetailView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Blog
 from django.contrib.auth.models import User
 
+
 class HomePage(TemplateView):
-    template_name='homepage.html'
+    template_name = 'homepage.html'
+
 
 class BlogList(ListView):
-    queryset=Blog.objects.all()
-    template_name='blog_list.html'
-    context_object_name='blog_list'
-    paginate_by=5
+    template_name = 'blog_list.html'
+    context_object_name = 'blog_list'
+    paginate_by = 5
 
-    #searching
+    # searching
     def get_queryset(self):
-        search=self.request.GET.get('search')
-        search_result=Blog.objects.filter(title__icontains=search)
-        return search_result
+        blog_title = self.request.GET.get('search')
+        if blog_title:
+            return Blog.objects.filter(title__icontains=blog_title)
+        return Blog.objects.all()
+
 
 class BlogDetail(DetailView):
-    model=Blog
-    template_name='blog_detail.html'
-    context_object_name='blog'
+    model = Blog
+    template_name = 'blog_detail.html'
+    context_object_name = 'blog'
+
 
 class AuthorDetail(DetailView):
-    model=User
-    template_name='author_detail.html'
-    context_object_name='author'
+    model = User
+    template_name = 'author_detail.html'
+    context_object_name = 'author'
